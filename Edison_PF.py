1f4c8585a112bb48cc6792813366bd66a0a3fe08
Kevin Wu <k8142311@gmail.com>
2015-07-26 18:10:21 +0000

    init basic file and write flow in Edison_PF.py

---
+++ Edison_PF.py
@@ -0,0 +1,20 @@
+
+
+def main():
+    
+        #indoorMap init
+    
+    while 1:
+        # get imu value
+
+        #another thread to ble advertise and scan
+
+        #particle move
+
+        #particle filter
+
+        #resample
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # intel_edison_pf
+
+   This is the project that we porting particle filter algorithm on intel Edison with python.
+
+   IMU part we use EcoMini and grove 3-axis compass
+   BLE part we use bluez which is supported by edison
+
+
